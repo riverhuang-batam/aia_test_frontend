f0763586fb21fcb64280b242a2b4ebe4f42feb4d
“riverhuang-batam” <riverhuang011@gmail.com>
2023-01-14 13:37:00 +0700

    get data from local server also add pagination and input html

--- src/App.js
+++ src/App.js
@@ -1,23 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import "./App.css";
+import axios from "axios";
 
 function App() {
+  const [datas, setDatas] = useState([]);
+  const getDatas = () => {
+    axios
+      .get("http://localhost:4000/")
+      .then((datas) => setDatas(datas.data))
+      .catch((err) => console.log(err));
+  };
+  useEffect(() => {
+    getDatas();
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {console.log(datas.items, "")}
+      <input />
+      {datas.items.map((item) => (
+        <div>
+          <img src={item.media.m} alt={item.media.m} />
+          <h1>{item.title}</h1>
+          <p>{item.tags}</p>
+          <p>{item.description}</p>
+        </div>
+      ))}
+      <nav aria-label="Page navigation example">
+        <ul class="pagination">
+          <li class="page-item">
+            <a class="page-link" href="#" aria-label="Previous">
+              <span aria-hidden="true">&laquo;</span>
+            </a>
+          </li>
+          <li class="page-item">
+            <a class="page-link" href="#">
+              1
+            </a>
+          </li>
+          <li class="page-item">
+            <a class="page-link" href="#">
+              2
+            </a>
+          </li>
+          <li class="page-item">
+            <a class="page-link" href="#">
+              3
+            </a>
+          </li>
+          <li class="page-item">
+            <a class="page-link" href="#" aria-label="Next">
+              <span aria-hidden="true">&raquo;</span>
+            </a>
+          </li>
+        </ul>
+      </nav>
     </div>
   );
 }
