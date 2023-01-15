737801c98b7b06a703c441b682ff5bd71889e516
“riverhuang-batam” <riverhuang011@gmail.com>
2023-01-15 13:55:38 +0700

    add paginatioon

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import { useState, useEffect } from "react";
-import "./App.css";
 import axios from "axios";
 
 function App() {
   const [datas, setDatas] = useState([]);
   const [tagInput, setTagInput] = useState("");
+  const [currentPages, setCurrentPages] = useState(1);
+  const [maxPageLimit, setMaxPageLimit] = useState(5);
+  const [minPageLimit, setMinPageLimit] = useState(1);
+  const pageNumberLimit = 5;
   const getDatas = () => {
     axios
       .get("http://localhost:4000/", { tag: "tags bro" })
@@ -13,16 +16,97 @@ function App() {
   };
   const searchDatas = (e) => {
     e.preventDefault();
+    setCurrentPages(1)
     axios
       .post("http://localhost:4000/", { tag: tagInput })
       .then((datas) => setDatas(datas.data))
       .catch((err) => console.log(err));
+  };
+  const handleNextClick = () => {
+    if (currentPages + 1 > maxPageLimit) {
+      setMaxPageLimit(maxPageLimit + pageNumberLimit);
+      setMinPageLimit(minPageLimit + pageNumberLimit);
+    }
+
+    setCurrentPages((prevNum) => prevNum + 1);
+  };
+  const handlePrevClick = () => {
+    if ((currentPages - 1) % pageNumberLimit === 0) {
+      setMaxPageLimit(maxPageLimit - pageNumberLimit);
+      setMinPageLimit(minPageLimit - pageNumberLimit);
+    }
+    setCurrentPages((prevNum) => prevNum - 1);
+  };
+  const handlePageClick = (pageNumber) => {
+    return setCurrentPages(pageNumber);
   };
   useEffect(() => {
     getDatas();
   }, []);
   return (
     <div>
+      <nav aria-label="Page navigation example">
+        <ul class="pagination">
+          {datas.items && currentPages > 1 && (
+            <li class="page-item" onClick={() => handlePrevClick()}>
+              <a class="page-link" aria-label="Previous">
+                <span aria-hidden="true">&laquo;</span>
+              </a>
+            </li>
+          )}
+          {datas.items && minPageLimit > 1 && (
+            <li class="page-item disabled">
+              <a class="page-link" href="#">
+                ...
+              </a>
+            </li>
+          )}
+
+          {datas.items?.map((data, index) => {
+            const pageNumber = index + 1;
+            console.log(
+              data,
+              "====",
+              index,
+              currentPages,
+              maxPageLimit,
+              pageNumber
+            );
+            if (pageNumber <= maxPageLimit && pageNumber >= minPageLimit) {
+              return (
+                <li
+                  class="page-item"
+                  onClick={() => handlePageClick(pageNumber)}
+                >
+                  <a
+                    class={`page-link ${
+                      currentPages == pageNumber && "active"
+                    }`}
+                    href="#"
+                  >
+                    {pageNumber}
+                  </a>
+                </li>
+              );
+            }
+          })}
+          {datas.items && datas.items.length > maxPageLimit && (
+            <li class="page-item disabled">
+              <a class="page-link" href="#">
+                ...
+              </a>
+            </li>
+          )}
+          {datas.items && currentPages < datas.items.length && (
+            <li class="page-item" onClick={() => handleNextClick()}>
+              <a class="page-link" aria-label="Next">
+                <span aria-hidden="true">&raquo;</span>
+              </a>
+            </li>
+          )}
+        </ul>
+      </nav>
+
       <form onSubmit={searchDatas}>
         <input
           id="search"
@@ -32,43 +116,27 @@ function App() {
         />
         <button type="submit">Search</button>
       </form>
-      {datas.items?.map((item) => (
-        <div>
-          <img src={item.media.m} alt={item.media.m} />
-          <h1>{item.title}</h1>
-          <p>{item.tags}</p>
-          <p>{item.description}</p>
-        </div>
-      ))}
-      <nav aria-label="Page navigation example">
-        <ul class="pagination">
-          <li class="page-item">
-            <a class="page-link" href="#" aria-label="Previous">
-              <span aria-hidden="true">&laquo;</span>
-            </a>
-          </li>
-          <li class="page-item">
-            <a class="page-link" href="#">
-              1
-            </a>
-          </li>
-          <li class="page-item">
-            <a class="page-link" href="#">
-              2
-            </a>
-          </li>
-          <li class="page-item">
-            <a class="page-link" href="#">
-              3
-            </a>
-          </li>
-          <li class="page-item">
-            <a class="page-link" href="#" aria-label="Next">
-              <span aria-hidden="true">&raquo;</span>
-            </a>
-          </li>
-        </ul>
-      </nav>
+      {datas.items && (
+        <img
+          src={datas.items[currentPages - 1]?.media.m}
+          alt={datas.items[currentPages - 1]?.media.m}
+        />
+      )}
+
+      <h3>
+        {datas.items && datas.items[currentPages - 1]?.title}
+        {console.log(datas)}
+      </h3>
+      {datas.items?.map((item) => {
+        return (
+          <div>
+            <img src={item.media.m} alt={item.media.m} />
+            <h1>{item.title}</h1>
+            <p>{item.tags}</p>
+            <p>{item.description}</p>
+          </div>
+        );
+      })}
     </div>
   );
 }
