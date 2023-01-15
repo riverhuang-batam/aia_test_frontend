7b7291abfa9cb7844f732b9e52019addb84ce125
“riverhuang-batam” <riverhuang011@gmail.com>
2023-01-15 16:19:14 +0700

    structure the view of the website and readme add the code of full list image view

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
-
+import "./App.css";
 function App() {
   const [datas, setDatas] = useState([]);
   const [tagInput, setTagInput] = useState("");
-  const [currentPages, setCurrentPages] = useState(1);
+  const [currentPage, setCurrentPage] = useState(1);
   const [maxPageLimit, setMaxPageLimit] = useState(5);
   const [minPageLimit, setMinPageLimit] = useState(1);
   const pageNumberLimit = 5;
@@ -16,47 +16,69 @@ function App() {
   };
   const searchDatas = (e) => {
     e.preventDefault();
-    setCurrentPages(1)
+    setCurrentPage(1);
     axios
       .post("http://localhost:4000/", { tag: tagInput })
       .then((datas) => setDatas(datas.data))
       .catch((err) => console.log(err));
   };
   const handleNextClick = () => {
-    if (currentPages + 1 > maxPageLimit) {
+    if (currentPage + 1 > maxPageLimit) {
       setMaxPageLimit(maxPageLimit + pageNumberLimit);
       setMinPageLimit(minPageLimit + pageNumberLimit);
     }
 
-    setCurrentPages((prevNum) => prevNum + 1);
+    setCurrentPage((prevNum) => prevNum + 1);
   };
   const handlePrevClick = () => {
-    if ((currentPages - 1) % pageNumberLimit === 0) {
+    if ((currentPage - 1) % pageNumberLimit === 0) {
       setMaxPageLimit(maxPageLimit - pageNumberLimit);
       setMinPageLimit(minPageLimit - pageNumberLimit);
     }
-    setCurrentPages((prevNum) => prevNum - 1);
+    setCurrentPage((prevNum) => prevNum - 1);
   };
   const handlePageClick = (pageNumber) => {
-    return setCurrentPages(pageNumber);
+    return setCurrentPage(pageNumber);
   };
   useEffect(() => {
     getDatas();
   }, []);
   return (
-    <div>
+    <div className="text-center">
+      <h2 className="mt-4">Imagx</h2>
+      <form onSubmit={searchDatas} className="form-group mb-4 mt-4 ">
+        <div className="input-group d-flex flex-row justify-content-center">
+          <div className="form-outline ">
+            <input
+              id="search"
+              type="text"
+              value={tagInput}
+              onChange={(event) => setTagInput(event.target.value)}
+              placeholder="search"
+              className="form-control"
+            />
+          </div>
+          <div className="">
+
+            <button type="submit" className="btn btn-primary">
+              Search
+            </button>
+          </div>
+        </div>
+      </form>
+
       <nav aria-label="Page navigation example">
-        <ul class="pagination">
-          {datas.items && currentPages > 1 && (
-            <li class="page-item" onClick={() => handlePrevClick()}>
-              <a class="page-link" aria-label="Previous">
+        <ul className="pagination justify-content-center">
+          {datas.items && currentPage > 1 && (
+            <li className="page-item" onClick={() => handlePrevClick()}>
+              <a className="page-link" aria-label="Previous">
                 <span aria-hidden="true">&laquo;</span>
               </a>
             </li>
           )}
           {datas.items && minPageLimit > 1 && (
-            <li class="page-item disabled">
-              <a class="page-link" href="#">
+            <li className="page-item disabled">
+              <a className="page-link" href="#">
                 ...
               </a>
             </li>
@@ -64,25 +86,17 @@ function App() {
 
           {datas.items?.map((data, index) => {
             const pageNumber = index + 1;
-            console.log(
-              data,
-              "====",
-              index,
-              currentPages,
-              maxPageLimit,
-              pageNumber
-            );
             if (pageNumber <= maxPageLimit && pageNumber >= minPageLimit) {
               return (
                 <li
-                  class="page-item"
+                  className="page-item"
                   onClick={() => handlePageClick(pageNumber)}
+                  key={pageNumber}
                 >
                   <a
-                    class={`page-link ${
-                      currentPages == pageNumber && "active"
+                    className={`page-link ${
+                      currentPage == pageNumber && "active"
                     }`}
-                    href="#"
                   >
                     {pageNumber}
                   </a>
@@ -91,15 +105,15 @@ function App() {
             }
           })}
           {datas.items && datas.items.length > maxPageLimit && (
-            <li class="page-item disabled">
-              <a class="page-link" href="#">
+            <li className="page-item disabled">
+              <a className="page-link" href="#">
                 ...
               </a>
             </li>
           )}
-          {datas.items && currentPages < datas.items.length && (
-            <li class="page-item" onClick={() => handleNextClick()}>
-              <a class="page-link" aria-label="Next">
+          {datas.items && currentPage < datas.items.length && (
+            <li className="page-item" onClick={() => handleNextClick()}>
+              <a className="page-link cursor-pointer" aria-label="Next">
                 <span aria-hidden="true">&raquo;</span>
               </a>
             </li>
@@ -107,36 +121,15 @@ function App() {
         </ul>
       </nav>
 
-      <form onSubmit={searchDatas}>
-        <input
-          id="search"
-          type="text"
-          value={tagInput}
-          onChange={(event) => setTagInput(event.target.value)}
-        />
-        <button type="submit">Search</button>
-      </form>
       {datas.items && (
         <img
-          src={datas.items[currentPages - 1]?.media.m}
-          alt={datas.items[currentPages - 1]?.media.m}
+          src={datas.items[currentPage - 1]?.media.m}
+          alt={datas.items[currentPage - 1]?.media.m}
+          className="mt-4"
         />
       )}
-
-      <h3>
-        {datas.items && datas.items[currentPages - 1]?.title}
-        {console.log(datas)}
-      </h3>
-      {datas.items?.map((item) => {
-        return (
-          <div>
-            <img src={item.media.m} alt={item.media.m} />
-            <h1>{item.title}</h1>
-            <p>{item.tags}</p>
-            <p>{item.description}</p>
-          </div>
-        );
-      })}
+      <h3>Title: {datas.items && datas.items[currentPage - 1]?.title}</h3>
+      <p>Tag: {datas.items && datas.items[currentPage - 1]?.tags}</p>
     </div>
   );
 }
